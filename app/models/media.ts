efea80fa1d40ceb401cdc91c02d31e130ad66230
Adrien Fernandes <adrien@fernandes.bzh>
2024-04-30 17:02:45 +0200

    core: Add media model

---
+++ app/models/media.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo, hasMany, manyToMany } from '@adonisjs/lucid/orm'
+import User from '#models/user'
+import type { BelongsTo, ManyToMany } from '@adonisjs/lucid/types/relations'
+
+export default class Media extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare url: string
+
+
+  @column()
+  declare userId: number
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @manyToMany(() => User, {
+    pivotTable: 'user_media_impressions',
+    pivotForeignKey: 'user_id',
+    pivotRelatedForeignKey: 'media_id',
+    pivotTimestamps: true,
+  })
+  declare impressions: ManyToMany<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1714488645264_create_media_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'media'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('url').notNullable()
+      table.foreign('user_id').references('users.id').onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
